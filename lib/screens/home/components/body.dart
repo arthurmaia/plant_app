1cf0def3b4a231d7272ab5e5e156082d78efa9d3
Arthur Maia <arthur.maia.dev@gmail.com>
2020-11-22 17:17:07 -0300

    Created home screen with components

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+import 'featured_plants.dart';
+import 'header_with_searchbox.dart';
+import 'recommends_plants.dart';
+import 'title_with_more_btn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
+          RecommendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
